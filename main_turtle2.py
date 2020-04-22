8c7617228a66a447e42c2117f5a2a870fbaf2320
Omar Silva <brampton@robo-geek.ca>
2020-04-22 18:48:29 -0400

    Add files via upload

---
+++ main_turtle2.py
@@ -0,0 +1,27 @@
+#Original code from Joseph Bakulikira
+#https://github.com/Josephbakulikira/Astar-PathFinding-Python
+
+import algorithm as AL
+
+
+
+def main():
+
+    grid = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+
+    StartNode = (0,0)
+    EndNode = (9,9)
+
+    layout = AL.algorithm(grid, StartNode, EndNode)
+    print(layout)
+
+main()
